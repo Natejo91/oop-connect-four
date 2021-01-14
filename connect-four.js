07561142ebed274e51af351dabeca0fd8d6b898b
donovanwaddle <donovanwaddle@gmail.com>
2021-01-14 16:45:53 -0600

    Added color-switching functionality to click targets.

--- connect-four.js
+++ connect-four.js
@@ -3,6 +3,7 @@ import Game from './game.js'
 let game = undefined;
 let board = document.getElementById('board-holder');
 let gameName = document.getElementById('game-name');
+let clickTargets = document.getElementById("click-targets");
 
 function updateUI() {
     if (game === undefined) {
@@ -10,6 +11,15 @@ function updateUI() {
     } else {
         board.classList.remove('is-invisible');
         gameName.innerHTML = game.getName();
+        const currentPlayer = game.currentPlayer;
+
+        if (currentPlayer === 1) {
+            clickTargets.classList.add("red");
+            clickTargets.classList.remove("black");
+        } else if (currentPlayer === 2) {
+            clickTargets.classList.remove('red');
+            clickTargets.classList.add('black');
+        }
     }
 }
 
@@ -19,20 +29,28 @@ window.addEventListener('DOMContentLoaded', () => {
     let newGameBtn = document.getElementById('new-game');
     let formName = document.getElementById('form-holder');
 
-    formName.addEventListener('keyup', () => {
+    formName.addEventListener('keyup', () => {                      //if both are empty, disable button
         if (p1Name.value.length > 0 && p2Name.value.length > 0) { // checking against undefined didnt work?
             newGameBtn.removeAttribute('disabled');   //might have to check
         } else {
             newGameBtn.setAttribute("disabled", 'true')
         }
     })
+
+
     newGameBtn.addEventListener('click', event => {
         event.preventDefault();
-        game = new Game(p1Name.value, p2Name.value);
+        game = new Game(p1Name.value, p2Name.value);     // THIS IS OUR GAME!
         p1Name.value = '';
         p2Name.value = '';
         newGameBtn.setAttribute('disabled', 'true');
         updateUI();
     })
+
+    clickTargets.addEventListener("click", event => {
+        game.playInColumn();
+        updateUI();
+    })
+
 
 })
